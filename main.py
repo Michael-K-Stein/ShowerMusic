49259efce8cb4c061cede186dba94499504d3149
Michael Kuperfish Steinberg <36902556+Michael-K-Stein@users.noreply.github.com>
2023-01-27 09:59:44 +0200

    Add entrypoint

---
+++ main.py
@@ -0,0 +1,13 @@
+"""
+Author: Michael K. Steinberg
+Created: 27/01/2023
+Purpose: Entrypoint for ShowerMusic sound analyzer.
+Name: main.py
+"""
+
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
