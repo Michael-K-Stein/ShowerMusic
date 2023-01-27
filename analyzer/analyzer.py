e6da04f70b471b97d0d02591b5d12b35163933d2
Michael Kuperfish Steinberg <36902556+Michael-K-Stein@users.noreply.github.com>
2023-01-27 10:15:31 +0200

    Implement exceptions

--- analyzer/analyzer.py
+++ analyzer/analyzer.py
@@ -4,7 +4,18 @@ Created: 27/01/2023
 Purpose: A sound analyzer.
 Name: analyzer.py
 """
+from io import BufferedReader
+from analyzer.exceptions import *
 
 
 class ShowerMusicAnalyzer:
-    pass
+    @staticmethod
+    def load_mp3(file):
+        if isinstance(file, str):
+            with open(file, 'rb') as f:
+                return ShowerMusicAnalyzer.load_mp3(f)
+        elif isinstance(file, BufferedReader):
+            return ShowerMusicAnalyzer.load_mp3(file.read())
+
+        if not isinstance(file, bytes):
+            raise ShowerMusicAnalyzerLoadException(f'\'{type(file)}\' is not supported for load_mp3.')
---
+++ analyzer/exceptions.py
@@ -0,0 +1,13 @@
+"""
+Author: Michael K. Steinberg
+Created: 27/01/2023
+Purpose: Exceptions for ShowerMusic analyzer.
+Name: exceptions.py
+"""
+
+
+class ShowerMusicAnalyzerException(Exception):
+    pass
+
+class ShowerMusicAnalyzerLoadException(ShowerMusicAnalyzerException):
+    pass
